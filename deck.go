90a3cbaf706c9f4b716c6ac531c0b9cf65c70dc1
gopikasula <gopikasula14@gmail.com>
2020-05-17 18:24:22 +0530

    io operations

--- deck.go
+++ deck.go
@@ -1,12 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+)
+
 type deck []string
 
 func newDeck() deck {
 	cards := deck{}
 
 	suits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	values := []string{"1", "2", "3", "4"}
+	values := []string{"one", "two", "three", "four"}
 
 	for _, suit := range suits {
 		for _, value := range values {
@@ -21,4 +29,33 @@ func (d deck) print() {
 	for _, card := range d {
 		println(card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("error", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() deck {
+	newPosition := rand.Intn(len(d) - 1)
+	for i := range d {
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+	return d
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("mycards.txt")
+	cards.shuffle()
 	cards.print()
 }
